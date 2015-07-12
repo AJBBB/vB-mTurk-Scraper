a62e4d0d0ec938daf35b85e2346ebb1cd01c7896
AJBBB <ajbbb@mail.com>
2015-07-12 01:55:26 -0400

    Minor Changes

--- vB-mTurk-Scraper.py
+++ vB-mTurk-Scraper.py
@@ -15,6 +15,7 @@ def get_thread():
     todays_thread = int(raw_input("Enter todays thread number: "))
     return todays_thread
 
+
 def create_url():
     selected_forum = forum_sel()
     todays_thread = get_thread()
@@ -37,12 +38,13 @@ def get_end():
 
 
 def get_eod():
-    # Should we print to a pastebin style log? Is it end of the day?
+    # Defines if the user wants to write to forumlog.txt
     e_o_d = bool(raw_input("Write to forumlog.txt? (True/False): "))
     return e_o_d
 
 
 def write_file(file_name, write_mode, file_text):
+    # Function to write to files easily
     text_file = open(file_name, write_mode)
     text_file.write(file_text)
     text_file.close()
@@ -55,34 +57,41 @@ def clear_update_htm(silent):
     if silent is False:  # Check if we are running in silent or not
         print "Cleared & Wrote css line to the file"
 
+
 def clear_update_txt(silent):
-    # Open the log, write over it with just css data
+    # Open the log, clear the file.
     write_file("forumlog.txt", "w", "")
     if silent is False:  # Check if we are running in silent or not
         print "Cleared forumlog.txt"
 
 
 def run_through(silent):
+    # Get the forum URL
     forum_url = create_url()
+    # Get the page to start from
     page_number = get_start()
+    # Get how many pages to run through
     last_page = get_end()
-    e_o_d = get_eod()
+    # Get if the user wants to write to forumlog.txt
+    end_of_day = get_eod()
 
     for pages in range(0, last_page):
-        page_number = page_number + 1  # increment page to check
+        # Increment page to check
+        page_number = page_number + 1
+        # Build the URL with page_number to scrape from
         hit_links = requests.get("http://" + str(forum_url) + str(page_number))
         data = hit_links.text
         soup = BeautifulSoup(data)
 
-        for link in soup.find_all('a'):  # finds all links on the page
+        for link in soup.find_all('a'):  # Finds all links on the page
             if link.get('href').startswith('https://www.mturk.com/mturk/preview?groupId='):
-                # Write to a log/webpage
+                # Write to the mturklinks.html file with all links
                 write_file("mturklinks.html", "a+",
                     "<a class=\"button\" href=\"" + link.get('href') +
                     "\"> Link </a><br><br>\n")
                 if silent is False:  # Check if we are running in silent or not
                     print "Wrote " + link.get('href') + " to the file."
-                if e_o_d is True:
+                if end_of_day is True:  # Check if user will write to forumlog
                     write_file("forumlog.txt", "a+", link.get('href') + "\n")
 
 def finish():
@@ -93,4 +102,4 @@ if __name__ == "__main__":
     clear_update_htm(True)  # Set to false to print output to console
     clear_update_txt(True)  # Set to false to print output to console
     run_through(True)  # Set to false to print output to console
-    finish()
+    finish()  # Prints out a finished message
